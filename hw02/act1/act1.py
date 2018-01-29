71aca2aa6030a3a9445a2ba5aee1e1e8c3985997
dp <ddp3945@rit.edu>
2018-01-29 18:39:22 -0500

    Add header parsing

--- hw02/act1/act1.py
+++ hw02/act1/act1.py
@@ -8,6 +8,7 @@ PORT = 82
 class httpSocket:
 
     httpSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    useragent = 'supersecretagent'
 
     def __init__(self, hostname, port):
         ip = socket.gethostbyname(hostname)
@@ -18,9 +19,21 @@ class httpSocket:
             exit(1)
 
     def recvHTTPHeader(self):
-        pass
+        header = ''
+        while True:
+            b = self.httpSock.recv(1)
+            header += b
+            if header.endswith('\n' * 2) or header.endswith('\r\n' * 2):
+                break
+        realHeader = {}
+        header = header.split('\r\n')
+        header = [hdr.strip().split(': ') for hdr in header if hdr != '']
+        realHeader['status'] = header[0][0].split(' ')[1]
+        for hdr in header[1:]:
+            realHeader[hdr[0]] = hdr[1]
+        return realHeader
 
-    def get(self, url, useragent='supersecretagent'):
+    def get(self, url, params={}, query={}, useragent=useragent):
         parsed = urlparse(url)
         payload = "GET {} HTTP/1.1\n".format(parsed.path)
         payload += "Host: {}\n".format(parsed.netloc)
@@ -29,13 +42,21 @@ class httpSocket:
         payload += "\n"
         self.httpSock.send(payload)
         header = self.recvHTTPHeader()
-        data = self.httpSock.recv(int(header.contentLength))
+        data = self.httpSock.recv(int(header['Content-Length']))
         return {'header': header, 'data': data}
+
+    def post(self, url, params={}, query={}, useragent=useragent):
+        parsed = urlparse(url)
+        payload = "POST {} HTTP/1.1\n".format(parsed.path)
+        payload += "Host: {}\n".format(parsed.netloc)
+        payload += "User-Agent: {}\n".format(useragent)
+        payload += "Accept: */*\n"
+        payload += "\n"
 
 
 def main():
     s = httpSocket(HOSTNAME, PORT)
-    s.get('http://csec380-core.csec.rit.edu:82/')
+    print s.get('http://csec380-core.csec.rit.edu:82/')
 
 
 if __name__ == '__main__':
