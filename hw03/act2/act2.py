901e4b5c5b414fa9e0cb5878a2e6b773d6ba186b
dp <ddp3945@rit.edu>
2018-02-22 15:59:19 -0500

    hw03 step2: code is being run 100 times by the container

---
+++ hw03/act2/act2.py
@@ -0,0 +1,19 @@
+import os
+
+from my_socks import *
+from bs4 import BeautifulSoup
+from multiprocessing.dummy import Pool as ThreadPool
+
+HOST = 'www.rit.edu'
+PORT = 443
+
+def main():
+    s = HTTPSocket(HOST, PORT)
+    s.create()
+
+    r = s.get('https://www.rit.edu/')
+    print r['data']
+
+
+if __name__ == '__main__':
+    main()
